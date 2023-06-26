546dec1f852a05aac534bb12ef1e42378e06493b
yukdawn <yukdawn@gmail.com>
2023-06-26 21:13:02 +0800

    fix:修复表单数据加载

--- serviceWorker.sh
+++ serviceWorker.sh
@@ -1,19 +1,21 @@
 #! /bin/sh
-read_dir(){
-  for file in `ls -a $1`
-  do
-    if [ -d $1"/"$file ]
-    then
-      if [[ $file != '.' && $file != '..' ]]
-      then
-          read_dir $1"/"$file
-      fi
-    else
-      echo $1"/"$file
-    fi
-  done
-}
-#测试目录 test
-cd ./dist/maoyetrpg && read_dir .
 
-# sed -i 's/text/replace/' file
+# 指定目录路径
+directory="./dist/maoyetrpg"
+
+# 遍历目录并生成JSON数组字符串
+json_array=$(find "$directory" -type f -exec realpath --relative-to="$directory" {} \; | \
+    awk '{print "\""$0"\","}' | tr '\n' ' ' | sed 's/,$/\n/')
+
+# 指定文件路径和替换的字符串
+file_path="./dist/maoyetrpg/serviceWorker.js"
+replacement="__REPLACE_WITH_JSON_ARRAY__"
+
+#把serviceworker.js复制到dist
+cp ./serviceWorker.js ./dist/maoyetrpg/
+
+# 将JSON数组字符串替换到文件的指定字符串
+sed -i "s@$replacement@$json_array@" "$file_path"
+
+# 覆盖到部署目录下
+rm -rf ./docs/* --exclude=CNAME && cp -r ./dist/maoyetrpg/* ./docs/
