24d2d700c3014b8bc3e5efe3748499b76ead390e
yukdawn <yukdawn@gmail.com>
2023-06-26 21:41:16 +0800

    ci

--- docs/serviceWorker.js
+++ docs/serviceWorker.js
@@ -1,4 +1,6 @@
 const cacheUrl = ["309.7fd157384c22f44e.js", "3rdpartylicenses.txt", "assets/data/dashboard.json", "assets/data/jobandskill.json", "assets/data/menu.json", "assets/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2", "assets/fonts/Material_Icons.css", "assets/i18n/en-US.json", "assets/i18n/zh-CN.json", "assets/i18n/zh-TW.json", "assets/images/avatar-default.jpg", "assets/images/avatar.jpg", "assets/images/matero.png", "favicon.ico", "index.html", "main.a08d98b7bbe23ae6.js", "polyfills.a68c3e8c4e8265c3.js", "runtime.0d5b12b5b946864d.js", "serviceWorker.js", "styles.da8023eae4e352f8.css", '/'];
+const pattern = /^(?!.*\.(js|css|png|jpg|jpeg|gif|ico|svg|woff2?|ttf|eot)$).*$/;
+var currentDomain = self.location.hostname;
 
 /* 监听安装事件，install 事件一般是被用来设置你的浏览器的离线缓存逻辑 */
 this.addEventListener('install', function (event) {
@@ -14,7 +16,7 @@ this.addEventListener('install', function (event) {
 
 self.addEventListener('fetch', function (event) {
   console.log('Handling fetch event for', event.request.url);
-  cacheUrl.some(url => event.request.url.includes(url)) &&
+  cacheUrl.some(url => event.request.url === (currentDomain + '/' + url) && pattern.test(event.request.url)) &&
     event.respondWith(
       // 打开以'font'开头的 Cache 对象。
       caches.open('v1').then(function (cache) {
